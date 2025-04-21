7358cbfa89ee4849bcab694aad4fb0ed0ec3c37a
Hi <rahulborule2506@gmail.com>
2025-04-21 15:32:00 +0530

    First commit

---
+++ src/main/java/com/rahul/LaborApp/LaborAppApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.LaborApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/LaborApp/LaborAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.LaborApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
